7c2522c76f511b0dd4ca9f95575398d6767b3149
vinayKumarReddy-mangalampenta <77964389+vinayKumarReddy-mangalampenta@users.noreply.github.com>
2021-10-12 19:36:36 +0530

    updated app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const initializeServerAndDatabase = async () => {
       filename: dbPath,
       driver: sqlite3.Database,
     });
-    app.listen(3000, () => {
+    app.listen(process.env.PORT || 3000, () => {
       console.log("Server running at http://localhost:3000/");
     });
   } catch (error) {
