f6baf0a878741ab49f5051a154c78da9de499ba8
vinayKumarReddy-mangalampenta <mangalapentavinaykumarreddy30@gmail.com>
2021-11-23 13:39:14 +0000

    my-server

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 let db = null;
-{
+
+const initializeServerAndDatabase = async () => {
   try {
     db = await open({
       filename: dbPath,
@@ -28,11 +29,11 @@ let db = null;
 
 initializeServerAndDatabase();
 
-;// register user api
+// register user api
 app.get("/", (req, res) => {
-  res.send("Hello World!!!")
+  res.send("Hello World!!!");
 });
-;app.post("/register/", async (request, response) => {
+app.post("/register/", async (request, response) => {
   const { username, password, name } = request.body;
 
   const hashedPasswd = await bcrypt.hash(password, 10);
@@ -200,7 +201,4 @@ app.post("/upload", authenticateToken, async (request, response) => {
   response.send({ msg: "updated" });
 });
 
-app.get('*', (req, res) => {
-  res.se"d"ile(path.join(__dirname+'/new-portal/build/index.html'));
-});
-" " module.exports = app;
+module.exports = app;
